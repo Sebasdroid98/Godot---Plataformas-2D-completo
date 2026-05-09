95fdc51cf332d8906af2c5ba51495083cf63cfee
Sebastian Zamora <zam98qui@gmail.com>
2026-05-09 10:17:21 -0500

    Se agrega musica de fondo, para el jugador, para la abeja, para la moneda

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,7 +1,21 @@
 extends Area2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var collision = $CollisionShape2D
+@onready var pickup_sound = $AudioStreamPlayer2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		# Se quita la moneda visualmente
 		body.add_coins()
-		queue_free()
+		animated_sprite.visible = false
+		collision.disabled = true
+		
+		# Se inicia el sonido
+		pickup_sound.play()
+		
+		# Se borra la moneda por completo
+		pickup_sound.finished.connect(_on_sound_finished)
+
+func _on_sound_finished():
+	queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,9 @@ const JUMP_VELOCITY = -260
 var coins = 0 # Contador de monedas
 @onready var hud = get_node("/root/Map/UI") # Referencia al UI
 
+# Variable para controlar el sonido al saltar
+@onready var jump_sound = $AudioStreamPlayer2D
+
 # Establecer monedas
 func add_coins():
 	coins += 1
@@ -22,6 +25,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
